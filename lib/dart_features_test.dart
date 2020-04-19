adb6991613bee93df7a7ac6eb3023e57726287f8
bit00 <akbrijesh@gmail.com>
2020-04-19 11:40:51 +0530

    test code for Runes

--- lib/dart_features_test.dart
+++ lib/dart_features_test.dart
@@ -1,3 +1,11 @@
 int calculate() {
   return 6 * 7;
 }
+
+dynamic multiplyMethod(int a, int b){
+  return a*b;
+}
+
+String getType(var b){
+  return b.runtimeType.toString();
+}
--- test/dart_features_test_test.dart
+++ test/dart_features_test_test.dart
@@ -5,4 +5,22 @@ void main() {
   test('calculate', () {
     expect(calculate(), 42);
   });
+
+  test('getType', () {
+    var two = 2;
+    print(getType(two));
+    expect(getType(3), 'int');
+  });
+
+  test('printRune', () {
+    var str = '\u{1f5fa}';
+    Runes charCodes =  Runes(str);
+    print(String.fromCharCodes(charCodes));
+    var someInt = 300;
+    print(' Hex of $someInt is: ${someInt.toRadixString(16)}');
+
+    var vv = '\u{1F601}';
+
+    print('abc${vv}defg');
+  });
 }
