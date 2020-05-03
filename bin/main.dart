1da730198b6a087fa73bd63d234dceeb58c1a9b6
bit00 <akbrijesh@gmail.com>
2020-05-03 17:07:15 +0530

    removed same numbers from 2 lists

--- bin/main.dart
+++ bin/main.dart
@@ -3,41 +3,41 @@ import 'dart:math';
 
 void main(List<String> arguments) {
   const len = 5;
-  const level = 7;
-
-  var l1 = <int>[len];
-  var l2 = <int>[len];
+  const randomLevel = 13;
+const startLevel = 6;
+  var l1 = <int>[];
+  var l2 = <int>[];
   
   final r = Random();
+  var resultList = <String>[];
 
-  l1 = [1,6,6,2,1,1];
-  l2 = [2,3,1,3,2,6];
+  // l1 = [1,6,6,2,1,1];
+  // l2 = [2,3,1,3,2,6];
   // l1 = List.generate(len, (index) => r.nextInt(level));
   // l2 = List.generate(len, (index) => r.nextInt(level));
 
-  var resultList = <int>[];
-  
-  for(var i=0;i<l1.length;i++){
-      resultList.add(l1[i] * l2[i]);
+  var t1, t2, comb;
+  while(l1.length<5){
+      t1 = r.nextInt(randomLevel);
+      if(t1<startLevel){
+        t1 = t1+startLevel;
+      }
+      t2 = r.nextInt(randomLevel);
+      if(t2<startLevel){
+        t2 = t2+startLevel;
+      }
+      comb = '$t1$t2';
+      if(!resultList.contains(comb)){
+        resultList.add(comb);
+        l1.add(t1);
+        l2.add(t2);
+      }else{
+        print('Skipping $comb');
+      }
   }
   
-
   print('list is $l1 , $l2 , $resultList');
 
-  //print(resultList.map();
 
 }
 
-List<int> listRandom(int maxNumber, int numberOfGenerations) {
-
-  final random = Random();
-  var currentOptions = List<int>.generate(maxNumber, (i) => i);
-
-  var list = List.generate(numberOfGenerations, (_) {
-    final index = random.nextInt(currentOptions.length);
-    final result = currentOptions[index];
-    currentOptions.removeAt(index);    
-    return result;
-  });
-  return list;
-}
