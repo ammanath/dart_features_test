e687d99c8be2506b7c60cfcea1460f5bfd68c5b3
bit00 <akbrijesh@gmail.com>
2020-05-04 09:43:05 +0530

    refactored to function

--- bin/main.dart
+++ bin/main.dart
@@ -1,43 +1,56 @@
-import 'package:dart_features_test/dart_features_test.dart' as dart_features_test;
 import 'dart:math';
 
 void main(List<String> arguments) {
   const len = 5;
-  const randomLevel = 13;
-const startLevel = 6;
+  const randomLevel = 7; //random generator upto this level (not inclusive)
+  const startLevel = 0; //lowest digit in the quiz
+  const allow12and21 = true;
+
+  List<List> l1 = getListForQuiz(len, randomLevel, startLevel, allow12and21);
+
+  print('list is ${l1[0]}  ');
+  print('value is ${l1[1]}');
+}
+
+List<List> getListForQuiz(
+    int len, int randomLevel, int startLevel, bool allow12and21) {
+  // const len = 5;
+  // const randomLevel = 7; //random generator upto this level (not inclusive)
+  // const startLevel = 0; //lowest digit in the quiz
+  // const allow12and21 = true;
+
   var l1 = <int>[];
   var l2 = <int>[];
-  
+
   final r = Random();
   var resultList = <String>[];
 
-  // l1 = [1,6,6,2,1,1];
-  // l2 = [2,3,1,3,2,6];
-  // l1 = List.generate(len, (index) => r.nextInt(level));
-  // l2 = List.generate(len, (index) => r.nextInt(level));
-
-  var t1, t2, comb;
-  while(l1.length<5){
-      t1 = r.nextInt(randomLevel);
-      if(t1<startLevel){
-        t1 = t1+startLevel;
-      }
-      t2 = r.nextInt(randomLevel);
-      if(t2<startLevel){
-        t2 = t2+startLevel;
-      }
+  var t1, t2, comb, effort = 0;
+  while (l1.length < len && effort < 100) {
+    effort++;
+    t1 = r.nextInt(randomLevel);
+    if (t1 < startLevel) {
+      t1 = t1 + startLevel;
+    }
+    t2 = r.nextInt(randomLevel);
+    if (t2 < startLevel) {
+      t2 = t2 + startLevel;
+    }
+    if (allow12and21) {
       comb = '$t1$t2';
-      if(!resultList.contains(comb)){
-        resultList.add(comb);
-        l1.add(t1);
-        l2.add(t2);
-      }else{
-        print('Skipping $comb');
+    } else {
+      if (t1 > t2) {
+        comb = '$t2$t1';
       }
+    }
+    if (!resultList.contains(comb)) {
+      resultList.add(comb);
+      l1.add(t1);
+      l2.add(t2);
+    } else {
+      print('Skipping $comb');
+    }
   }
-  
-  print('list is $l1 , $l2 , $resultList');
 
-
+  return [l1, l2];
 }
-
